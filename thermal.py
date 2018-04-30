7e33e5e5dbf7014916b360e8a46aedc2cfd1b75b
bridge <bridgeaffiliation@gmail.com>
2018-04-30 23:02:06 +0800

    thermal

---
+++ thermal.py
@@ -0,0 +1,7 @@
+#!usr/bin/python3
+# -*- coding: utf-8 -*-  
+ 
+file = open("/sys/class/thermal/thermal_zone0/temp")  
+temp = float(file.read()) / 1000   
+file.close()  
+print("CPU Temperature: %.2f °C" %temp)
